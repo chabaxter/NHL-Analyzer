9c5cdf516df2581c79182ffe9464f3c8f3eb11b9
Charles Baxter <95829637+chabaxter@users.noreply.github.com>
2022-02-25 21:11:41 -0500

    Add files via upload
    Move functions to Visualizer.py

---
+++ Visualizer.py
@@ -0,0 +1,63 @@
+import json
+import numpy as np
+from matplotlib import pyplot as plt
+
+import NHL_API
+
+
+def shooting_data_visual(first_year=2000, last_year=2020, season_type=2):
+    shooting_data_seasons = []
+
+    try:
+        f = open(str(first_year) + '-' + str(last_year) + '_' + str(season_type) + '_' + "shooting.json", "r")
+        shooting_data_seasons = json.load(f)
+    except FileExistsError as e:
+        for year in range(first_year, last_year + 1):
+            shooting_plays_by_game = NHL_API.for_season(year, season_type, NHL_API.get_shooting_plays)
+            shooting_data_seasons.append(shooting_plays_by_game)
+
+        f = open(str(first_year)+'-'+str(last_year)+'_'+str(season_type)+'_'+"shooting.json", "w")
+        json.dump(shooting_data_seasons, f, indent=4)
+
+    length = 200
+    l_2 = length / 2
+    width = 100
+    w_2 = width / 2
+
+    goals = np.zeros((width, length))
+    saves = np.zeros((width, length))
+    misses = np.zeros((width, length))
+    goal_percent = np.zeros((width, length))
+    for season in shooting_data_seasons:
+        for game in season:
+            for play in game:
+                try:
+                    if play['result']['event'] == 'Shot':
+                        saves[int(play['coordinates']['y'] + w_2)][int(play['coordinates']['x'] + l_2)] += 1
+                    elif play['result']['event'] == 'Goal':
+                        goals[int(play['coordinates']['y'] + w_2)][int(play['coordinates']['x'] + l_2)] += 1
+                    elif play['result']['event'] == 'Miss':
+                        misses[int(play['coordinates']['y'] + w_2)][int(play['coordinates']['x'] + l_2)] += 1
+                except KeyError as e:
+                    pass
+
+    for i in range(width):
+        for j in range(length):
+            if goals[i][j] == 0:
+                goal_percent[i][j] = 0
+            elif saves[i][j] == 0:
+                goal_percent[i][j] = 1
+            else:
+                goal_percent[i][j] = goals[i][j] / (saves[i][j] + goals[i][j])
+
+    plt.title('saves')
+    plt.imshow(saves)
+    plt.show()
+
+    plt.title('goals')
+    plt.imshow(goals)
+    plt.show()
+
+    plt.title('percent')
+    plt.imshow(goal_percent)
+    plt.show()
--- main.py
+++ main.py
@@ -1,70 +1,9 @@
-import numpy as np
-from matplotlib import pyplot as plt
-import json
 import NHL_API
-
-
-def shooting_data_visual():
-    first_year = 2000
-    second_year = 2020
-    season_type = 2
-    shooting_data_seasons = []
-
-    # for year in range(first_year, second_year + 1):
-    #     shooting_plays_by_game = for_season(year, season_type, get_shooting_plays)
-    #     shooting_data_seasons.append(shooting_plays_by_game)
-    #
-    # f = open(str(first_year)+'-'+str(second_year)+'_'+str(season_type)+'_'+"shooting.json", "w")
-    # json.dump(shooting_data_seasons, f, indent=4)
-
-    f = open(str(first_year)+'-'+str(second_year)+'_'+str(season_type)+'_'+"shooting.json", "r")
-    shooting_data_seasons = json.load(f)
-
-    length = 200
-    l_2 = length / 2
-    width = 100
-    w_2 = width / 2
-
-    goals = np.zeros((width, length))
-    saves = np.zeros((width, length))
-    misses = np.zeros((width, length))
-    goal_percent = np.zeros((width, length))
-    for season in shooting_data_seasons:
-        for game in season:
-            for play in game:
-                try:
-                    if play['result']['event'] == 'Shot':
-                        saves[int(play['coordinates']['y'] + w_2)][int(play['coordinates']['x'] + l_2)] += 1
-                    elif play['result']['event'] == 'Goal':
-                        goals[int(play['coordinates']['y'] + w_2)][int(play['coordinates']['x'] + l_2)] += 1
-                    elif play['result']['event'] == 'Miss':
-                        misses[int(play['coordinates']['y'] + w_2)][int(play['coordinates']['x'] + l_2)] += 1
-                except KeyError as e:
-                    pass
-
-    for i in range(width):
-        for j in range(length):
-            if goals[i][j] == 0:
-                goal_percent[i][j] = 0
-            elif saves[i][j] == 0:
-                goal_percent[i][j] = 1
-            else:
-                goal_percent[i][j] = goals[i][j] / (saves[i][j] + goals[i][j])
-
-    plt.title('saves')
-    plt.imshow(saves)
-    plt.show()
-
-    plt.title('goals')
-    plt.imshow(goals)
-    plt.show()
-
-    plt.title('percent')
-    plt.imshow(goal_percent)
-    plt.show()
+import Visualizer
 
 
 def main():
+    Visualizer.shooting_data_visual(2000, 2020, 2)
     player = NHL_API.get_player(8477474)
     print(player)
 
